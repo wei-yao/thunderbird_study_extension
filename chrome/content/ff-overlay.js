ac220b49001cd3fa7ee12e40cbd5f14217866705
weiyao <1033789547@qq.com>
2014-12-18 20:49:10 +0800

    thunderbird extension hello world

---
+++ chrome/content/ff-overlay.js
@@ -0,0 +1,36 @@
+var myfirstext = {
+  onLoad: function() {
+    // initialization code
+    this.initialized = true;
+    this.strings = document.getElementById("myfirstext-strings");
+  },
+
+  onMenuItemCommand: function(e) {
+    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                  .getService(Components.interfaces.nsIPromptService);
+    promptService.alert(window, this.strings.getString("helloMessageTitle"),
+                                this.strings.getString("helloMessage"));
+  },
+
+  onToolbarButtonCommand: function(e) {
+    // just reuse the function above.  you can change this, obviously!
+    myfirstext.onMenuItemCommand(e);
+  }
+};
+
+window.addEventListener("load", function () { myfirstext.onLoad(); }, false);
+
+
+myfirstext.onFirefoxLoad = function(event) {
+  document.getElementById("contentAreaContextMenu")
+          .addEventListener("popupshowing", function (e) {
+    myfirstext.showFirefoxContextMenu(e);
+  }, false);
+};
+
+myfirstext.showFirefoxContextMenu = function(event) {
+  // show or hide the menuitem based on what the context menu is on
+  document.getElementById("context-myfirstext").hidden = gContextMenu.onImage;
+};
+
+window.addEventListener("load", function () { myfirstext.onFirefoxLoad(); }, false);
---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,11 @@
+// Default preference values. These are accessible via the preferences system
+// or via the optional chrome/content/options.xul preferences dialog.
+
+// These are here as an example only. Please remove them or rename them to
+// something useful.
+pref("extensions.myfirstext.boolpref", false);
+pref("extensions.myfirstext.intpref", 0);
+pref("extensions.myfirstext.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.myfirstext@jen.zed.description", "chrome://myfirstext/locale/overlay.properties");
